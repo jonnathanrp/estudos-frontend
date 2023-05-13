620edec551a2b982b6a280a89385103f9aa7f822
jon3dr <jonnathan.rodriguessp@gmail.com>
2023-05-13 09:07:43 -0300

    Estudando Js - página modelo em html.

---
+++ estudos-js/modelo/assets/js/main.js
@@ -0,0 +1,6 @@
+/*
+alert(1);
+console.log("Bem-vindo(a)! ao meu projeto pessoal.");
+console.log('Pode me chamar de "John". Vou acompanhá-lo durante seu passeio! ;^)');
+console(35, 15.85, 'Luiz Otávio De Novo');
+*/
