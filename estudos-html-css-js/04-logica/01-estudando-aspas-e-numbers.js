95ba6ed7df11a0cd8ebfd91a41211f4ad4d3a14b
jon3dr <jonnathan.rodriguessp@gmail.com>
2023-08-23 17:40:21 -0300

    comitando estudos

---
+++ estudos-html-css-js/04-logica/01-estudando-aspas-e-numbers.js
@@ -0,0 +1,19 @@
+console.log('Jonnathan'); //String
+console.log("Rodrigues"); //String
+console.log("Jonnathan 'Rodrigues'"); //String
+console.log('"Pacheco"'); //String
+console.log(`'Jonnathan' "Rodrigues"`); /*utilizado para template strings*/
+console.log(12356, 15.85, 'Jonnathan Rodrigues'); // No Javascript todos números são considerados numbers, não importando seu ponto flutuante.
+
+console.log('Olá mundo! Meu nome é Jonnathan. Tenho' ,33, 'anos estou aprendendo Javascript');
+
+//Formas de escrever comentários em linha
+/*Formas de comentários em bloco*/
+
+/*
+Linha 1
+Linha 2
+Linha 3
+Linha 4
+Linha 5
+*/
