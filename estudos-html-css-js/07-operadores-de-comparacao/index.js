35a29cc901e9a23c8a5113711c2a09999a06f46a
jon3dr <jonnathan.rodriguessp@gmail.com>
2023-08-25 07:12:47 -0300

    Operadores de comparação
    Uma revisão sobre operadores de comparação.

--- estudos-html-css-js/07-operadores-de-comparacao/index.js
+++ estudos-html-css-js/07-operadores-de-comparacao/index.js
@@ -20,4 +20,5 @@ const diferente = 3 != 1;
 const num1 = 10;
 const num2 = 11;
 const comp = num1 === num2;
-console.log(comp);
+const diferenteEstrito = num1 !== num1;
+console.log(diferenteEstrito);
