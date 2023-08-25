5640b1ea446175efaf85d352b380b04fde93dd0f
jon3dr <jonnathan.rodriguessp@gmail.com>
2023-08-25 07:07:36 -0300

    Estudando Operadores Lógicos
    Uma revisão sobre operadores lógicos.

---
+++ estudos-html-css-js/07-operadores-de-comparacao/index.js
@@ -0,0 +1,23 @@
+/* 
+Operadores de comparação
+
+> maior que
+>= maior ou igual que
+< menor que ou igual a
+<= menor ou igual que
+== igualdade (valor
+=== estrita (valor e tipo)
+!= diferente (valor) *****
+!== diferente estrito (valor e tipo)
+*/
+const maior = 1 > 5;
+const maiorIgual = 10 >= 7;
+const menor = -3 < 2;
+const menorIgual = 21 <= 17;
+const igual = 8 == 8;
+const igualValor = 9 === 10;
+const diferente = 3 != 1;
+const num1 = 10;
+const num2 = 11;
+const comp = num1 === num2;
+console.log(comp);
